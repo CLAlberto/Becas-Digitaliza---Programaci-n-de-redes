ddecce1819273862f8de669f79d12b865b757703
Alberto López <Albertolc1991@gmail.com>
2019-01-26 01:04:29 +0100

    Actividad 3 finalizada

---
+++ Introducción a Python (I)/03_personal-info.py
@@ -0,0 +1,10 @@
+#Crear un script que pregunte por el nombre, el apellido, el teléfono, la ciudad, y la edad. Cada valor ha de ser guardado en una variable diferente.
+nombre = input("Introduce tu nombre: ")
+apellido = input("Introduce tu apellido: ")
+telefono = input("Introduce tu número de teléfono: ")
+ciudad = input("Introduce tu ciudad: ")
+edad = input("Introduce tu edad: ")
+
+
+#Imprimir, por pantalla, toda la infomarción coleccionada en una frase que combine la información con coherencia (una oración que no sólo sean los datos recogidos).
+print("-------------------\nMi nombre es:  ", nombre,"\nMi apellido es:",apellido,"\nMi número de teléfono es: ",telefono,"\nVivo en: ",ciudad,"\nTengo: ",edad,"años.")
