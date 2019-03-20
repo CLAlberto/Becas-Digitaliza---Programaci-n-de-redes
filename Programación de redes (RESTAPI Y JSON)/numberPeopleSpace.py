cc0322f1e5a6d1020c18f8a1a5b48c01981c8a30
Unknown <albertolc1991@gmail.com>
2019-03-20 15:22:18 +0100

    Work In Progress

---
+++ Programación de redes (RESTAPI Y JSON)/numberPeopleSpace.py
@@ -0,0 +1,8 @@
+"""Tarea 2: Crear una APP para el ISS (International Space Station)
+
+Crear un archivo llamado: numberPeopleSpace.py
+Crear una app para saber el número de personas que hay en el espacio. Podéis buscar la información en la siguiente URL:
+http://open-notify.org/Open-Notify-API/ (Links to an external site.)Links to an external site.
+Subir el archivo a vuestro repositorio en GitHub."""
+
+
---
+++ Programación de redes (RESTAPI Y JSON)/sunsetSunrise.py
@@ -0,0 +1,12 @@
+"""Tarea 1: Crear una APP para la salida y puesta del sol
+
+Crear una aplicación para acceder a la API de las horas de salida y puesta del sol de la siguiente URL:
+
+https://api.sunrise-sunset.org/json?lat=48.8584&lng=2.2945 (Enlaces a un sitio externo.)Enlaces a un sitio externo.
+En el ejemplo, se calcula para París, Francia.
+
+Crear un archivo llamado: sunsetSunrise.py
+Mostrar las horas para una latitud y longitud concretas (que no sea la del ejemplo; por ejemplo, donde vivís). Incluir un print con texto (lo que queráis), con los valores de latitud y longitud, y con las horas obtenidas.
+Subir el archivo a vuestro repositorio en GitHub."""
+
+
