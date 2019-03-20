802811da50c0ecf167e12e93f9b5e806264c27cd
Alberto López <Albertolc1991@gmail.com>
2019-03-21 00:25:07 +0100

    APP para la salida y puesta del sol

--- Programación de redes (RESTAPI Y JSON)/sunsetSunrise.py
+++ Programación de redes (RESTAPI Y JSON)/sunsetSunrise.py
@@ -9,4 +9,20 @@ Crear un archivo llamado: sunsetSunrise.py
 Mostrar las horas para una latitud y longitud concretas (que no sea la del ejemplo; por ejemplo, donde vivís). Incluir un print con texto (lo que queráis), con los valores de latitud y longitud, y con las horas obtenidas.
 Subir el archivo a vuestro repositorio en GitHub."""
 
+import requests
+print("Calculo de la salida y puesta del sol dependiendo de la localización")
+geocode_location = input("Introduce el pueblo, ciudad o pais a calcular:")
+geocode_url = "http://open.mapquestapi.com/geocoding/v1/address"
+
+geocode_querystring = {"key":"iBL788R2Cz0miic5kAoMmNS8bC9eFwOt","location":geocode_location}
+geocode_json_data = requests.request("GET", geocode_url,params=geocode_querystring).json()
+
+url = "https://api.sunrise-sunset.org/json"
+
+querystring = {"lat":geocode_json_data["results"][0]["locations"][0]["latLng"]["lat"],"lng":geocode_json_data["results"][0]["locations"][0]["latLng"]["lng"]}
+
+json_data = requests.request("GET", url, params=querystring).json()
+print("----------------------------------")
+print("Para",geocode_location, "con latitud y longitud" ,list(querystring.values()), "se ha calculado que:","\nLa salida del sol será a las:",json_data['results']['sunrise'],"\nLa puesta de sol será a las:",json_data['results']['sunset'])
+print("----------------------------------")
 
