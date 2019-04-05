965557385f7b790ee3026e32f69caeb74c408dd4
Alberto López <Albertolc1991@gmail.com>
2019-04-05 16:45:00 +0200

    Update "Actividad 5 finalizada"
    Implementado funciones, diccionarios, y excepciones

--- Introducción a Python (I)/05_calculator.py
+++ Introducción a Python (I)/05_calculator.py
@@ -1,32 +1,68 @@
-#Crear las sentencias necesarias para recoger dos números a través del terminal
+class Calculadora:
+    def __init__(self, a, b):
+        self.firstnumber = a
+        self.secondnumber = b
+
+    def Suma(self):
+        print("El resultado de sumar", self.firstnumber, "+", self.secondnumber, "es igual a:",
+              (self.firstnumber + self.secondnumber),"\n",self.firstnumber,"+",self.secondnumber,"=",(self.firstnumber + self.secondnumber))
+
+    def Resta(self):
+        print("El resultado de restar", self.firstnumber, "-", self.secondnumber, "es igual a:",
+              (self.firstnumber - self.secondnumber),"\n",self.firstnumber,"-",self.secondnumber,"=",(self.firstnumber - self.secondnumber))
+
+    def Multiplicacion(self):
+        print("El resultado de multiplicar", self.firstnumber, "*", self.secondnumber, "es igual a:",
+              (self.firstnumber * self.secondnumber),"\n",self.firstnumber,"*",self.secondnumber,"=",(self.firstnumber * self.secondnumber))
+
+    def Division(self):
+        try:
+            print("El resultado de dividir", self.firstnumber, "/", self.secondnumber, "es igual a:",
+                  (self.firstnumber / self.secondnumber),"\n",self.firstnumber,"/",self.secondnumber,"=",(self.firstnumber/self.secondnumber))
+        except Exception:
+            print(("No se puede dividir entre cero!\n"))
+
+    def Exponencial(self):
+        print("El resultado de elevar", self.firstnumber, "^", self.secondnumber, "es igual a:",
+              (self.firstnumber ** self.secondnumber),"\n",self.firstnumber,"^",self.secondnumber,"=",(self.firstnumber**self.secondnumber))
 
 
-#Integrar funcionalidades de suma, multiplicación, división, y exponencial
-salir=False
-while salir!=True:
-    print("------------\n1) SUMAR\n2) MULTIPLICAR\n3) DIVIDIR\n4) POTENCIA\n5) SALIR\n------------\n")
-    opcion = int(input("Ingrese el número de su opcion:"))
-    if opcion > 0 and opcion < 6:
-        if opcion < 5:
-            a = int(input("Introduce el primer número para la operación a calcular: "))
-            b = int(input("Introduce el segundo número para la operación a calcular: "))
-            if opcion == 1:
-                sumar = a + b
-                print("El resultado de sumar",a,"+",b,"es igual a: ",sumar)
-            elif opcion == 2:
-                multiplicar = a * b
-                print("El resultado de multiplicar", a, "*", b, "es igual a: ", multiplicar)
-            elif opcion == 3:
-                dividir = a / b
-                print("El resultado de dividir", a, "/", b, "es igual a: ", dividir)
-            elif opcion == 4:
-                potencia = a ** b
-                print("El resultado de elevar", a, "^", b, "es igual a: ", potencia)
+print("--------------------\nOperaciones a realizar\n--------------------\n1 - Sumar\n2 - Restar\n3 - Multiplicar\n4 - Dividir\n5 - Exponencial\n--------------------")
+while True:
+    try:
+        opcion = int(input("Seleccione operacion: "))
+        assert 0 < opcion < 6
+    except ValueError:
+        print("No es un número entero! Por favor introduce un número entero.")
+    except AssertionError:
+        print("Por favor, introduce un número entero entre el 1 y el 5 (incluidos)")
+    else:
+        break
 
-        else:
-            print("------------\nGracias por usar la calculadora.\nHasta luego!\n------------\n")
-            salir=True
-#Permitir escoger el modo de operación de forma manual (el usuario ha de introducir un número para que sepa qué operación realizar)
+while True:
+    try:
+        primero = float(input("Inserta un Numero: "))
+    except ValueError:
+        print("Parece que no ha introducido un número correcto! Por favor introduce un número correcto (entero o decimal)")
+    else:
+        break
 
+while True:
+    try:
+        segundo = float(input("Inserta otro Numero: "))
+    except ValueError:
+        print("Parece que no ha introducido un número correcto! Por favor introduce un número correcto (entero o decimal)")
+    else:
+        break
+Objeto = Calculadora(primero, segundo)
 
-#Realizar las operaciones e imprimir el valor por pantalla.
+operaciones = {
+    1: Objeto.Suma,
+    2: Objeto.Resta,
+    3: Objeto.Multiplicacion,
+    4: Objeto.Division,
+    5: Objeto.Exponencial
+}
+
+print("--------------------\n")
+operaciones[opcion]()
